8f1b413bf18f757d73e2f1642527de3ad37d415e
DWproduction5 <dwproduction5@DWproduction5.local>
2019-04-24 10:31:38 -0500

    Updates

---
+++ precache-manifest.a144d49cb47f9d76ad1faca0a5edd2cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3410144aa7cc6b94eb0682a7a1cf90",
+    "url": "/react_todos/index.html"
+  },
+  {
+    "revision": "f384dd8b60911e9d11ab",
+    "url": "/react_todos/static/css/main.bb158adf.chunk.css"
+  },
+  {
+    "revision": "9df31dd52d9d72e607f2",
+    "url": "/react_todos/static/js/2.0cc863b6.chunk.js"
+  },
+  {
+    "revision": "f384dd8b60911e9d11ab",
+    "url": "/react_todos/static/js/main.816d35cb.chunk.js"
+  },
+  {
+    "revision": "581610d75ff754e4c862",
+    "url": "/react_todos/static/js/runtime~main.aa32d5c5.js"
+  }
+]);
